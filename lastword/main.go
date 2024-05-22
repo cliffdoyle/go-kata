ae581cd4f98e6b90de4d4f36b3038ea1df4f166f
clomollo <oyoocliff471@gmail.com>
2024-05-22 11:01:53 +0300

    Program that takes a string and displays its last word

---
+++ lastword/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) !=1 || len(args) == 0 {
+		return
+	}
+	args1 := args[0]
+
+	word := ""
+	for i:=len(args1)-1;i > 0; i--{
+		if word == "" && string(args1[i]) == " " {
+			continue
+		}
+		if word != "" && string(args1[i]) == " " {
+			break
+		}
+		word = string(args1[i]) + word
+	}
+
+	fmt.Println(word)
+}
