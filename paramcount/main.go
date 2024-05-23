96f19b0dfefc08ef0445c9f28170946fe2e68c99
clomollo <oyoocliff471@gmail.com>
2024-05-23 15:43:31 +0300

    Added z01PrintRune

--- paramcount/main.go
+++ paramcount/main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
 	args := os.Args[1:]
 
 	count := len(args)
-	fmt.Println(count)
+
+	var result []rune
+
+	if count == 0 {
+		return
+	} else {
+		for count > 0 {
+			digit := count % 10
+			result = append([]rune{rune('0' + digit)}, result...)
+			count /= 10
+		}
+	}
+
+	for _, char := range result {
+		z01.PrintRune(char)
+	}
+	z01.PrintRune('\n')
 }
