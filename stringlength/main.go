9cd74293864026fb2b686f6bd1f1077645eb203c
clomollo <oyoocliff471@gmail.com>
2024-05-23 15:19:28 +0300

    Added PrintRune

--- stringlength/main.go
+++ stringlength/main.go
@@ -1,12 +1,34 @@
 package main
 
-import "fmt"
+import (
+	//"fmt"
 
-func Strln(s string) int {
-	return len(s)
+	"github.com/01-edu/z01"
+)
+
+func StrLen(s string) int {
+	length := len(s)
+
+	return length
 }
 
 func main() {
-	l := Strln("Hello World!")
-	fmt.Println(l)
+	l := StrLen("Hello World!")
+
+	var result []rune
+
+	if l == 0 {
+		z01.PrintRune(0)
+	} else {
+		for l > 0 {
+			digit := l % 10
+			result = append([]rune{rune('0' + digit)}, result...)
+			l /= 10
+
+		}
+	}
+	for _, char := range result {
+		z01.PrintRune(char)
+	}
+	z01.PrintRune('\n')
 }
